15529ce97fe902d0a354f4e3a502d75acc5f2253
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-26 16:59:09 +0900

    post.jsファイルを作成して読み込む

---
+++ app/javascript/post.js
@@ -0,0 +1,6 @@
+function post (){
+  const submit = document.getElementById("submit_btn");
+  });
+};
+
+window.addEventListener('load', post); //「ページが読み込まれたら」を実現するためには、window.addEventListener('load', 【実際に行う処理】);という記述
