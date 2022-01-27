09bf827898bef10fa9788ac4d7da8016d8b54aae
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-27 19:24:10 +0900

    入力フォームに入力されている文字数を表示できるように記述

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,7 +1,9 @@
 function count (){
   const articleText = document.getElementById("article_text");
   articleText.addEventListener("keyup", () => {   //キーボード操作があった場合に、イベントが発火。そのときに用いるイベントハンドラーがkeyup
-    console.log(articleText.value.length);
+    const countValue = articleText.value.length;  //入力されている文字数をカウントした結果を取得
+    const charNum = document.getElementById("char_num");  //実際に文字を置き換えたい部分の要素を取得。「◯文字」と表示される要素には、char_numというidが付与されているため、getElementByIdで取得。
+    charNum.innerHTML = `${countValue}文字`        //取得した要素に対して、${countVal}文字を、innerHTMLを用いて置き換え。
   })
 }
  window.addEventListener('load', count);
