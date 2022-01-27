78b26210c32c53107b4b17e737a146d2cf17b9d0
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-27 19:09:49 +0900

    入力フォームへ文字を入力するたびに、または文字を消すたびに、コンソール上にその時存在している文字列が出力されるように実装

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,4 +1,7 @@
 function count (){
-  console.log("文字カウント機能の実装")
+  const articleText = document.getElementById("article_text");
+  articleText.addEventListener("keyup", () => {   //キーボード操作があった場合に、イベントが発火。そのときに用いるイベントハンドラーがkeyup
+    console.log(articleText.value);
+  })
 }
  window.addEventListener('load', count);
