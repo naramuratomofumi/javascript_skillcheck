ed7fe180270aa85fec1cc80525524943c1389c58
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-27 19:13:47 +0900

    入力した文字数をコンソールに出力するように実装

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,7 +1,7 @@
 function count (){
   const articleText = document.getElementById("article_text");
   articleText.addEventListener("keyup", () => {   //キーボード操作があった場合に、イベントが発火。そのときに用いるイベントハンドラーがkeyup
-    console.log(articleText.value);
+    console.log(articleText.value.length);
   })
 }
  window.addEventListener('load', count);
